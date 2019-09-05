e84d7cb77e86dd9a507b5e97394d54a8de874326
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-09-05 09:22:42 -0600

    Initial Commit. Basic File Organization

---
+++ PairGenerator/Models/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  PairGenerator
+//
+//  Created by Madison Kaori Shino on 9/4/19.
+//  Copyright © 2019 Madi S. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class Person {
+    
+}
